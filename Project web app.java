ea29940b5c9d23f5ab43e4a28885759d798ddd18
manoj.paleti11 <manojprabhakar.paleti@gmail.com>
2022-09-05 12:21:02 +0530

    web app source code

---
+++ Project web app.java
@@ -0,0 +1,23 @@
+public class Armstrong {
+
+    public static void main(String[] args) {
+
+        int low = 999, high = 99999;
+
+        for(int number = low + 1; number < high; ++number) {
+
+            if (checkArmstrong(number))
+                System.out.print(number + " ");
+        }
+    }
+
+    public static boolean checkArmstrong(int num) {
+        int digits = 0;
+        int result = 0;
+        int originalNumber = num;
+
+        // number of digits calculation
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            ++digits;
+        }
